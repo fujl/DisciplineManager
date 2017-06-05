06eccb0d6d054c9290ef9a363575f21c9efcbfba
fujl <jialin.fu@longmaster.com.cn>
2017-06-05 17:08:35 +0800

    Initial Commit

---
+++ DisciplineManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DisciplineManager
+//
+//  Created by fujl-mac on 2017/6/5.
+//  Copyright © 2017年 fujl-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DisciplineManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DisciplineManager
+//
+//  Created by fujl-mac on 2017/6/5.
+//  Copyright © 2017年 fujl-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
