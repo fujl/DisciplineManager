98ff0447acb6d774f3c14c091bfd20c7b0ee3cf5
apple <apple@appletekiimac.local>
2017-08-02 00:49:44 +0800

    push

---
+++ DisciplineManager/Manager/Push/DMPushManager.h
@@ -0,0 +1,15 @@
+//
+//  DMPushManager.h
+//  DisciplineManager
+//
+//  Created by apple on 17/8/2.
+//  Copyright © 2017年 fujl-mac. All rights reserved.
+//
+
+#import "DMBaseManager.h"
+
+@interface DMPushManager : DMBaseManager
+
+- (void)didFinishLaunchingWithOptions:(NSDictionary *)launchOptions application:(UIApplication *)application;
+
+@end
---
+++ DisciplineManager/Manager/Push/DMPushManager.m
@@ -0,0 +1,18 @@
+//
+//  DMPushManager.m
+//  DisciplineManager
+//
+//  Created by apple on 17/8/2.
+//  Copyright © 2017年 fujl-mac. All rights reserved.
+//
+
+#import "DMPushManager.h"
+#import "BPush.h"
+
+@implementation DMPushManager
+
+- (void)didFinishLaunchingWithOptions:(NSDictionary *)launchOptions application:(UIApplication *)application {
+    
+}
+
+@end
