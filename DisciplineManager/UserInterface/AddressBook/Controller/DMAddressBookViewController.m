0876c403d1eef0fb297d9ad4e677a315ffa08ae3
fujl <jialin.fu@longmaster.com.cn>
2017-08-16 19:27:18 +0800

    地址

--- DisciplineManager/UserInterface/AddressBook/Controller/DMAddressBookViewController.m
+++ DisciplineManager/UserInterface/AddressBook/Controller/DMAddressBookViewController.m
@@ -32,11 +32,11 @@
         make.left.width.equalTo(self.view);
         make.bottom.equalTo(self.view);
     }];
+    [self setRefreshView];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    [self setRefreshView];
 }
 
 #pragma mark - getters and setters
