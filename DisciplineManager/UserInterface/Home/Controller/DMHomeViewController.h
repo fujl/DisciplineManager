2c7bcd69b2757be339e3bd4e0a98d42da39c4491
apple <apple@appletekiiMac.local>
2017-09-11 20:13:19 +0800

    主页

---
+++ DisciplineManager/UserInterface/Home/Controller/DMHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMHomeViewController.h
+//  DisciplineManager
+//
+//  Created by apple on 2017/9/11.
+//  Copyright © 2017年 fujl-mac. All rights reserved.
+//
+
+#import "DMLinearViewController.h"
+
+@interface DMHomeViewController : DMLinearViewController
+
+@end
---
+++ DisciplineManager/UserInterface/Home/Controller/DMHomeViewController.m
@@ -0,0 +1,22 @@
+//
+//  DMHomeViewController.m
+//  DisciplineManager
+//
+//  Created by apple on 2017/9/11.
+//  Copyright © 2017年 fujl-mac. All rights reserved.
+//
+
+#import "DMHomeViewController.h"
+
+@interface DMHomeViewController ()
+
+@end
+
+@implementation DMHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
