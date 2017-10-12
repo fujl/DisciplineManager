5b8ace26aa8a9109b44f757ba27a621d45cfe9a8
fujl <jialin.fu@longmaster.com.cn>
2017-10-12 17:53:09 +0800

    统计

--- DisciplineManager/UserInterface/Home/View/Repast/DMMealBarView.m
+++ DisciplineManager/UserInterface/Home/View/Repast/DMMealBarView.m
@@ -9,12 +9,14 @@
 #import "DMMealBarView.h"
 #import "DMRepastTitleView.h"
 #import "ZFChart.h"
+#import "DisciplineManager-Bridging-Header.h"
+#import "DisciplineManager-Swift.h"
 
-@interface DMMealBarView () <ZFGenericChartDataSource, ZFBarChartDelegate>
+@interface DMMealBarView () <ChartViewDelegate>
 
 @property (nonatomic, strong) DMRepastTitleView *titleView;
 @property (nonatomic, strong) UIView *contentView;
-@property (nonatomic, strong) ZFBarChart * barChart;
+@property (nonatomic, strong) BarChartView * barChartView;
 
 @end
 
@@ -25,7 +27,7 @@
     if (self) {
         [self addSubview:self.titleView];
         [self addSubview:self.contentView];
-        [self.contentView addSubview:self.barChart];
+        [self.contentView addSubview:self.barChartView];
         
         [self.titleView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.left.equalTo(self);
@@ -39,7 +41,7 @@
             make.top.equalTo(self.titleView.mas_bottom);
             make.height.equalTo(@(200));
         }];
-        [self.barChart mas_makeConstraints:^(MASConstraintMaker *make) {
+        [self.barChartView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.edges.equalTo(self.contentView);
         }];
         
@@ -64,15 +66,15 @@
     return _contentView;
 }
 
-- (ZFBarChart *)barChart {
-    if (!_barChart) {
-        _barChart = [[ZFBarChart alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 200)];
-        _barChart.dataSource = self;
-        _barChart.delegate = self;
+- (BarChartView *)barChartView {
+    if (!_barChartView) {
+        _barChartView = [[BarChartView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 200)];
+//        _barChart.dataSource = self;
+        _barChartView.delegate = self;
         //_barChart.topicLabel.text = @"xx小学各年级人数";
-        _barChart.unit = @"票";
+//        _barChart.unit = @"票";
     }
-    return _barChart;
+    return _barChartView;
 }
 
 - (void)setType:(RepastType)type {
@@ -85,100 +87,16 @@
 }
 
 - (void)strokePath {
-    [self.barChart strokePath];
+//    [self.barChart strokePath];
 }
 
-#pragma mark - ZFGenericChartDataSource
-
-- (NSArray *)valueArrayInGenericChart:(ZFGenericChart *)chart{
-    NSMutableArray *values = [[NSMutableArray alloc] init];
-    for (DMStatVoteModel *svModel in self.dishs) {
-        [values addObject:[NSString stringWithFormat:@"%zd", svModel.total]];
-    }
-    return values;
-//    return @[@"123", @"256", @"300", @"283", @"490", @"236"];
+#pragma mark - ChartViewDelegate
+- (void)chartValueSelected:(ChartViewBase * __nonnull)chartView entry:(ChartDataEntry * __nonnull)entry highlight:(ChartHighlight * __nonnull)highlight {
+    NSLog(@"chartValueSelected");
 }
 
-- (NSArray *)nameArrayInGenericChart:(ZFGenericChart *)chart{
-    NSMutableArray *names = [[NSMutableArray alloc] init];
-    for (DMStatVoteModel *svModel in self.dishs) {
-        [names addObject:svModel.dishesName];
-    }
-    return names;
-//    return @[@"一年级", @"二年级", @"三年级", @"四年级", @"五年级", @"六年级"];
-}
-
-- (NSArray *)colorArrayInGenericChart:(ZFGenericChart *)chart {
-    return @[ZFMagenta];
-}
-
-- (CGFloat)axisLineMaxValueInGenericChart:(ZFGenericChart *)chart {
-    CGFloat max = 0;
-    for (DMStatVoteModel *svModel in self.dishs) {
-        if (svModel.total > max) {
-            max = svModel.total;
-        }
-    }
-    return max;
-}
-
-//- (CGFloat)axisLineMinValueInGenericChart:(ZFGenericChart *)chart{
-//    return 50;
-//}
-
-- (NSUInteger)axisLineSectionCountInGenericChart:(ZFGenericChart *)chart{
-    return 10;
-}
-
-//- (NSInteger)axisLineStartToDisplayValueAtIndex:(ZFGenericChart *)chart{
-//    return -7;
-//}
-
-- (void)genericChartDidScroll:(UIScrollView *)scrollView{
-    NSLog(@"当前偏移量 ------ %f", scrollView.contentOffset.x);
-}
-
-#pragma mark - ZFBarChartDelegate
-
-//- (CGFloat)barWidthInBarChart:(ZFBarChart *)barChart{
-//    return 40.f;
-//}
-
-//- (CGFloat)paddingForGroupsInBarChart:(ZFBarChart *)barChart{
-//    return 40.f;
-//}
-
-//- (id)valueTextColorArrayInBarChart:(ZFGenericChart *)barChart{
-//    return ZFBlue;
-//}
-
-- (NSArray *)gradientColorArrayInBarChart:(ZFBarChart *)barChart{
-    ZFGradientAttribute * gradientAttribute = [[ZFGradientAttribute alloc] init];
-    gradientAttribute.colors = @[(id)ZFRed.CGColor, (id)ZFWhite.CGColor];
-    gradientAttribute.locations = @[@(0.5), @(0.99)];
-    
-    return [NSArray arrayWithObjects:gradientAttribute, nil];
-}
-
-- (void)barChart:(ZFBarChart *)barChart didSelectBarAtGroupIndex:(NSInteger)groupIndex barIndex:(NSInteger)barIndex bar:(ZFBar *)bar popoverLabel:(ZFPopoverLabel *)popoverLabel{
-    NSLog(@"第%ld组========第%ld个",(long)groupIndex,(long)barIndex);
-    
-    //可在此处进行bar被点击后的自身部分属性设置,可修改的属性查看ZFBar.h
-    bar.barColor = ZFGold;
-    bar.isAnimated = YES;
-    //    bar.opacity = 0.5;
-    [bar strokePath];
-    
-    //可将isShowAxisLineValue设置为NO，然后执行下句代码进行点击才显示数值
-    //    popoverLabel.hidden = NO;
-}
-
-- (void)barChart:(ZFBarChart *)barChart didSelectPopoverLabelAtGroupIndex:(NSInteger)groupIndex labelIndex:(NSInteger)labelIndex popoverLabel:(ZFPopoverLabel *)popoverLabel{
-    NSLog(@"第%ld组========第%ld个",(long)groupIndex,(long)labelIndex);
-    
-    //可在此处进行popoverLabel被点击后的自身部分属性设置
-    //    popoverLabel.textColor = ZFSkyBlue;
-    //    [popoverLabel strokePath];
+- (void)chartValueNothingSelected:(ChartViewBase * __nonnull)chartView {
+    NSLog(@"chartValueNothingSelected");
 }
 
 @end
---
+++ DisciplineManager/chart.swift
@@ -0,0 +1,9 @@
+//
+//  chart.swift
+//  DisciplineManager
+//
+//  Created by fujl-mac on 2017/10/12.
+//  Copyright © 2017年 fujl-mac. All rights reserved.
+//
+
+import Foundation
